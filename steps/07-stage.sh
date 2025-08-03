95ae2636dd8c72ce3982c197bfa8bd2a32544a9f
Ma3X <ir.ma3x@gmail.com>
2025-08-03 10:31:19 +0330

    .

--- steps/07-stage.sh
+++ steps/07-stage.sh
@@ -31,11 +31,11 @@ rm -f "$STAGING/include/PRESUBMIT.py"
 
 case "$OS" in
   android|linux)
-    mv "$BUILD/libpdfium.a" "$STAGING_LIB"
+    mv "$BUILD/obj/libpdfium.a" "$STAGING_LIB"
     ;;
 
   mac|ios)
-    mv "$BUILD/libpdfium.a" "$STAGING_LIB"
+    mv "$BUILD/obj/libpdfium.a" "$STAGING_LIB"
     ;;
 
   emscripten)
@@ -47,9 +47,9 @@ case "$OS" in
     ;;
 
   win)
-    mv "$BUILD/pdfium.dll.lib" "$STAGING_LIB"
+    mv "$BUILD/obj/pdfium.dll.lib" "$STAGING_LIB"
     mkdir -p "$STAGING_BIN"
-    mv "$BUILD/pdfium.lib" "$STAGING_BIN"
+    mv "$BUILD/obj/pdfium.lib" "$STAGING_BIN"
     [ "$IS_DEBUG" == "true" ] && mv "$BUILD/pdfium.dll.pdb" "$STAGING_BIN"
     ;;
 esac
